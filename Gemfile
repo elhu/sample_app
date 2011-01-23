2808d1fab99cf5f2fb0649796afdbba9ae5c9f52
elhu <boisard.v@gmail.com>
2011-01-23 11:42:28 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+group :development do
+      gem 'rspec-rails', '2.4.0'
+end
+
+group :test do
+      gem 'rspec', '2.4.0'
+      gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '00ed412479809edb25471113f5a4c1aac17749cf62daad5059de882d8b7baf72b06dcb0405cdbe0483c9596a36333e3a3a4f930fcd5f5828faaba5c9cc69f6ef'
