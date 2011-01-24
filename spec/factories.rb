92026a4c272f3d54aa55a9637b70cddf9ea867db
elhu <boisard.v@gmail.com>
2011-01-24 11:54:43 +0000

    User model with encrypted passwords, and use show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                     "Vincent Boisard"
+  user.email                    "boisard.v@gmail.com"
+  user.password                 "foobar"
+  user.password_confirmation    "foobar"
+end
+
