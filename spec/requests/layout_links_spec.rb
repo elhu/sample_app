d18839d3682376c172fdc7f0fbd13947bdc39264
elhu <boisard.v@gmail.com>
2011-01-24 17:54:52 +0000

    Done with sign in/out

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -5,7 +5,7 @@ describe "LayoutLinks" do
     get '/'
     response.should have_selector('title', :content => "Home")
   end
-
+  
   it "should have a Contact page at '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
@@ -40,5 +40,34 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign up")
   end
 
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+                                    :content => "Sign in")
+    end
+  end
+
+  describe "when signed in" do
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email, :with => @user.email
+      fill_in :password, :with => @user.password
+      click_button
+    end
+    
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signout_path,
+                                    :content => "Sign out")
+    end
+
+    it "should have a profile link" do
+          visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                    :content => "Profile")
+    end
+  end
 end
 
